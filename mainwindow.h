d5f3df8215ca65582288fbacf7e9f20d793ff568
AdonsKali <derevianckin@mail.ru>
2026-06-13 23:21:41 +0700

    feat(mainwindow): implement basic file selection and chart display functionality
    - Add file system navigation using QTreeView and QTableView
    - Implement chart display area with support for line and bar charts
    - Add chart type selection and grayscale toggle options
    - Implement PDF export functionality for the displayed chart
    - Add error handling and status bar messages

--- mainwindow.h
+++ mainwindow.h
@@ -2,23 +2,41 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
 #include <QFileSystemModel>
 #include <QTreeView>
 #include <QTableView>
 #include <QSplitter>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QComboBox>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QLabel>
+#include <memory>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class QChartView;
+class IOCContainer;
+class IData;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
     void on_selectionChangedSlot(const QItemSelection &selected, const QItemSelection &deselected);
+    void onFileSelected(const QItemSelection &selected, const QItemSelection &deselected);
     void onChartTypeChanged(int index);
     void onGrayscaleToggled(bool checked);
     void onPrintToPDF();
@@ -28,22 +46,28 @@ private:
     void loadAndDisplayChart(const QString& filePath);
     void clearChartArea();
     void showErrorMessage(const QString& title, const QString& message);
-    QString m_currentFilePath;
-    bool m_isChartDisplayed;
-    QVector<QPointF> m_currentData;
-    static constexpr int MIN_CHART_HEIGHT = 400;
+
+    Ui::MainWindow *ui;
+
     QFileSystemModel *rightPartModel;
     QFileSystemModel *leftPartModel;
     QTreeView *treeView;
     QTableView *tableView;
+
     QSplitter *rightSplitter;
     QWidget *chartPanel;
     QVBoxLayout *chartPanelLayout;
-    QVBoxLayout *chartLayout;
     QHBoxLayout *chartControlsLayout;
     QComboBox *chartTypeCombo;
     QCheckBox *grayscaleCheckBox;
     QPushButton *printButton;
+    QVBoxLayout *chartLayout;
+
+    QString m_currentFilePath;
+    bool m_isChartDisplayed;
+    QVector<QPointF> m_currentData;
+
+    static constexpr int MIN_CHART_HEIGHT = 400;
 };
 
 #endif
