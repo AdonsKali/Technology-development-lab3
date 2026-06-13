06d9d7cdc5ddc077ee764986985dd933cfbd877d
AdonsKali <derevianckin@mail.ru>
2026-06-13 23:37:04 +0700

    refactor(ioc): improve code style and modernize syntax
    - Use inline variable initialization for better readability
    - Remove unnecessary blank lines to reduce clutter
    - Add missing semicolons for consistency
    - Update constructor syntax in lambda functions

--- ioc/container.h
+++ ioc/container.h
@@ -5,41 +5,40 @@
 #include <memory>
 #include <map>
 
-
 class IOCContainer
 {
 private:
-    static int s_nextTypeId;
-    
+    inline static int s_nextTypeId = 0;
+
     template<typename T>
     static int GetTypeID() {
         static int typeId = s_nextTypeId++;
         return typeId;
     }
-    
+
     class FactoryRoot
     {
     public:
         virtual ~FactoryRoot() {}
     };
-    
+
     template<typename T>
     class CFactory : public FactoryRoot
     {
         std::function<std::shared_ptr<T>()> m_functor;
-        
+
     public:
         CFactory(std::function<std::shared_ptr<T>()> functor)
             : m_functor(functor)
         {}
-        
+
         std::shared_ptr<T> GetObject() {
             return m_functor();
         }
     };
     
     std::map<int, std::shared_ptr<FactoryRoot>> m_factories;
-    
+
 public:
     template<typename T>
     std::shared_ptr<T> GetObject() {
@@ -51,22 +50,22 @@ public:
         auto factory = std::static_pointer_cast<CFactory<T>>(factoryBase);
         return factory->GetObject();
     }
-    
+
     template<typename TInterface, typename TConcrete, typename... TArguments>
     void RegisterFactory() {
         auto functor = [this]() -> std::shared_ptr<TInterface> {
             return std::make_shared<TConcrete>(GetObject<TArguments>()...);
         };
-        m_factories[GetTypeID<TInterface>()] = 
+        m_factories[GetTypeID<TInterface>()] =
             std::make_shared<CFactory<TInterface>>(functor);
     }
-    
+
     template<typename TInterface>
     void RegisterInstance(std::shared_ptr<TInterface> instance) {
-        m_factories[GetTypeID<TInterface>()] = 
+        m_factories[GetTypeID<TInterface>()] =
             std::make_shared<CFactory<TInterface>>([instance]() { return instance; });
     }
-    
+
     template<typename TInterface, typename TConcrete>
     void RegisterInstance() {
         auto instance = std::make_shared<TConcrete>();
