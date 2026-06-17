560ccdf38e225cb3fdb6e4ddf1987acd8b95df1a
AdonsKali <75972026+AdonsKali@users.noreply.github.com>
2026-06-17 11:11:24 +0700

    Update jsondataadapter.h
    - Add new format in Time "xx:yy:yyyy"
    - Refacrot old format data

--- data/jsondataadapter.h
+++ data/jsondataadapter.h
@@ -7,6 +7,7 @@
 #include <QJsonObject>
 #include <QFile>
 #include <QDebug>
+#include <QDateTime>
 
 class JSONDataAdapter : public IData
 {
@@ -34,15 +35,44 @@ public:
         }
         QByteArray data = file.readAll();
         QJsonDocument doc = QJsonDocument::fromJson(data);
+        
         if (doc.isArray()) {
             QJsonArray array = doc.array();
             for (int i = 0; i < array.size(); ++i) {
                 QJsonObject obj = array[i].toObject();
-                if (obj.contains("x") && obj.contains("y")) {
+                
+                if (obj.contains("Time") && obj.contains("Value")) {
+                    QString timeStr = obj["Time"].toString();
+                    double y = obj["Value"].toDouble();
+                    
+                    QDateTime dt;
+                    
+                    dt = QDateTime::fromString(timeStr, "dd.MM.yyyy");
+                    if (!dt.isValid()) {
+                        dt = QDateTime::fromString(timeStr, "yyyy-MM-dd");
+                    }
+                    if (!dt.isValid()) {
+                        dt = QDateTime::fromString(timeStr, "dd.MM.yyyy hh:mm");
+                    }
+                    if (!dt.isValid()) {
+                        dt = QDateTime::fromString(timeStr, "yyyy-MM-dd hh:mm:ss");
+                    }
+                    
+                    if (dt.isValid()) {
+                        m_points.append(QPointF(dt.toSecsSinceEpoch(), y));
+                        qDebug() << "Parsed date:" << timeStr << "->" << dt.toString() 
+                                 << "timestamp:" << dt.toSecsSinceEpoch() << "value:" << y;
+                    } else {
+                        qDebug() << "Failed to parse date:" << timeStr << "using index:" << i;
+                        m_points.append(QPointF(i, y));
+                    }
+                }
+                else if (obj.contains("x") && obj.contains("y")) {
                     double x = obj["x"].toDouble();
                     double y = obj["y"].toDouble();
                     m_points.append(QPointF(x, y));
-                } else if (obj.contains("value")) {
+                } 
+                else if (obj.contains("value")) {
                     double y = obj["value"].toDouble();
                     m_points.append(QPointF(i, y));
                 }
@@ -58,6 +88,8 @@ public:
                 }
             }
         }
+        
+        qDebug() << "JSONDataAdapter: loaded" << m_points.size() << "points";
         return !m_points.isEmpty();
     }
 };
